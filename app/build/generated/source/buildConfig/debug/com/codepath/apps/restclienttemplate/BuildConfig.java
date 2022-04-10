500fc1a124fb5255d8625b5bd740100c79a15a5a
Cecil <yuuuno32@gmail.com>
2022-04-10 13:11:32 +0530

    w3 ep3

---
+++ app/build/generated/source/buildConfig/debug/com/codepath/apps/restclienttemplate/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codepath.apps.restclienttemplate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codepath.apps.restclienttemplate";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String CONSUMER_KEY = "2LAMIBV2e1bJo4CUpIHS3kS56";
+  // Field from default config.
+  public static final String CONSUMER_SECRET = "JwxBLvDedICH8viOE3MiLW5KCAbzHsmQ8oFkzhSFRwGMnEi3oQ";
+}
